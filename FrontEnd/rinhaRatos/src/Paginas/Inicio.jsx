47b6d3fce75914dd17a2856ef5acfd1e4d637d7e
joaopug <charmandercharmandesco@gmail.com>
2025-10-25 18:47:16 -0300

    Agora funciona

--- FrontEnd/rinhaRatos/src/Paginas/Inicio.jsx
+++ FrontEnd/rinhaRatos/src/Paginas/Inicio.jsx
@@ -1,11 +1,12 @@
+import Dink from "../FotosPerfil/Dink.png";
 
 function Inicio() {
   return (
     <>
-      <div>
+      <div >
         <h1>Rinha de Ratos 2000</h1>
         <div>
-          <img src="C:\Users\Gamer moment\Documents\Github\rinha-de-ratos-Web\FrontEnd\rinhaRatos\src\FotosPerfil\Dink_(Cubolândia).PNG.png"/>
+          <img src={Dink}/>
         </div>
       </div>
     </>
