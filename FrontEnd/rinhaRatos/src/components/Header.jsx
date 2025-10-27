b2190b80b8f24b6e84da27aa297d0ea9575007b6
joaopug <41847866+joaopug@users.noreply.github.com>
2025-10-27 19:20:55 -0300

    Header agora usa svg como imagem e alterado o nome

--- FrontEnd/rinhaRatos/src/components/Header.jsx
+++ FrontEnd/rinhaRatos/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import RatoEsgoto from "../assets/RatoEsgoto.png";
-import Logo from "../assets/Logo.png";
+import Logo from "../assets/Logo_Coliseu_dos_Ratos.svg";
 import "../css/Header.css";
 
 export default function Header() {
