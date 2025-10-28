68bfa8fe701584de75169c6290f75674aa4437e6
joaopug <charmandercharmandesco@gmail.com>
2025-10-28 11:58:55 -0300

    Parte do 2 do commit da tela de login

--- FrontEnd/rinhaRatos/src/pages/Login.jsx
+++ FrontEnd/rinhaRatos/src/pages/Login.jsx
@@ -1,11 +1,16 @@
 import CaixaLogCdstroPass from "../components/CaixaLogCdstroPass";
+import Logo from "../assets/Logo_Coliseu_dos_Ratos.svg"
 import '../css/Login.css';
 
 export default function Login() {
   return (
     <>
-      <div className="container"></div>
-      <CaixaLogCdstroPass />
+      <div className="login-container">
+        <img src={Logo} className="logo" />
+        <div className="caixa">
+          <CaixaLogCdstroPass />
+        </div>
+      </div>
     </>
   );
 }
