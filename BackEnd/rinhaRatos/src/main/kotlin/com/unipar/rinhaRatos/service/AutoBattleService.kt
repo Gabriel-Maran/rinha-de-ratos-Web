6870c0d45237c8cf1ee9f639106646112886f365
Gabriel Maran <gabriel.enzo.libero.maran@gmail.com>
2025-11-03 20:52:41 -0300

    Comentando futuros updates

--- BackEnd/rinhaRatos/src/main/kotlin/com/unipar/rinhaRatos/service/AutoBattleService.kt
+++ BackEnd/rinhaRatos/src/main/kotlin/com/unipar/rinhaRatos/service/AutoBattleService.kt
@@ -7,4 +7,16 @@ class AutoBattleService(
     val idBatalha: Long,
     private val batalhaRepository: BatalhaRepository
 ) {
+    // private val batalha = batalhaRepository.findById(idBatalha)
+    // private var jogador1 = null
+    // private var jogador2 = null
+    // private var rato1 = null
+    // private var rato2 = null
+    //
+    // fun defineValues(): Boolean{
+    //     if(batalha.isEmpty) return false
+    //     val batalhaPresent = batalha.get()
+    //     jogador1 = batalhaPresent.jogador1
+    // }
+    // Recebe ID da batalha
 }
