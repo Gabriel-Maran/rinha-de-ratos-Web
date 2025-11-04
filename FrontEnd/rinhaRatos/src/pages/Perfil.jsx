3d58d92895090f28081a382c61feb9f97cfb542b
joaopug <charmandercharmandesco@gmail.com>
2025-11-04 17:52:49 -0300

    Colocado o Perfil no Routes e inserido mais conteúdo nele.

--- FrontEnd/rinhaRatos/src/pages/Perfil.jsx
+++ FrontEnd/rinhaRatos/src/pages/Perfil.jsx
@@ -1,9 +1,27 @@
+import { useState } from "react";
 import Header from "../components/comuns/Header"
+import "../css/Corpo.css";
 
-export default function Perfil(){
-    return(
+export default function Perfil() {
+
+    const [opcaoAtivada, setOpcaoAtivada] = useState("Meus ratos");
+    const botoes = ["Meus ratos", "Batalhas", "Ranking", "Loja"];
+    return (
         <>
-        <Header />
+            <Header />
+            <div className="corpo-container">
+                <div className={"opcoes"}>
+                    {botoes.map((botao) => (
+                        <button
+                            key={botao}
+                            className={opcaoAtivada == botao ? "opcaoAtiva" : ""}
+                            onClick={() => setOpcaoAtivada(botao)}
+                        >
+                            {botao}
+                        </button>
+                    ))}
+                </div>
+            </div>
         </>
     )
 }
