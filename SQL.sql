0048c74650bc5b21211bf9d2833c062950cb2b9a
Gabriel-Maran <gabriel.enzo.libero.maran@gmail.com>
2025-11-06 09:42:35 -0300

    Update SQL.sql

--- SQL.sql
+++ SQL.sql
@@ -1,13 +1,14 @@
 /* ------------------------------------------------------- PRIMEIRO EXECUTE O BACK END, DEPOIS DE ESTES INSERTS NO PGADMIN -------------------------------------------------------*/
 
--- Classes (ajustes menores para consistência)
+-- Volta os HPs para os valores originais
 INSERT INTO classes (nome_classe, descricao, apelido, str_min, str_max, agi_min, agi_max, hps_min, hps_max, int_min, int_max, def_min, def_max) VALUES
-('Rato de Esgoto', 'Sobrevive nas sombras, usando lama e toxinas para corroer defesas; brutal e imprevisível.','O Fedoroso', 55, 70, 35, 55, 120, 158, 20, 40, 34, 50),
-('Rato de Hospital','Mistura cura e combate: regenera e protege aliados, sacrificando-se quando preciso.','Sarna-Médico', 25, 40, 30, 50, 135, 175, 50, 70, 30, 45),
-('Rato de Laboratório','Explosão experimental: ganha buffs monstruosos com alto risco — recompensa ou desastre.','Cobaia 07', 40, 60, 40, 60, 85, 137, 60, 80, 20, 35),
-('Rato de Fazenda','Tanque rústico: força bruta e golpes pesados que esmagam a defesa inimiga.','Tratorzinho', 60, 80, 25, 45, 137, 175, 20, 40, 40, 60),
-('Rato de Cassino','Apostador audaz: trapaças e jogadas de alto risco que podem virar a partida.','Aposta Alta', 35, 55, 45, 65, 75, 125, 55, 75, 25, 35),
-('Rato de Biblioteca','Estratégia fria: lê padrões, reduz defesas e abre janelas para ataques precisos.','Folha-rato', 30, 50, 45, 60, 87, 137, 65, 90, 25, 35);
+('Rato de Esgoto', 'Sobrevive nas sombras, usando lama e toxinas para corroer defesas; brutal e imprevisível.','O Fedoroso', 55, 70, 35, 55, 50, 65, 20, 40, 35, 50),
+('Rato de Hospital','Mistura cura e combate: regenera e protege aliados, sacrificando-se quando preciso.','Sarna-Médico', 25, 40, 30, 50, 55, 70, 50, 70, 30, 45),
+('Rato de Laboratório','Explosão experimental: ganha buffs monstruosos com alto risco — recompensa ou desastre.','Cobaia 07', 40, 60, 40, 60, 35, 55, 60, 80, 20, 35),
+('Rato de Fazenda','Tanque rústico: força bruta e golpes pesados que esmagam a defesa inimiga.','Tratorzinho', 60, 80, 25, 45, 55, 70, 20, 40, 40, 60),
+('Rato de Cassino','Apostador audaz: trapaças e jogadas de alto risco que podem virar a partida.','Aposta Alta', 35, 55, 45, 65, 30, 50, 55, 75, 25, 35),
+('Rato de Biblioteca','Estratégia fria: lê padrões, reduz defesas e abre janelas para ataques precisos.','Folha-rato', 30, 50, 45, 60, 35, 55, 65, 90, 25, 35);
+
 
 INSERT INTO loja_Pacotes (nome_pacote, mousecoin_quantidade, preco_brl) VALUES
 ('Pacote de Moedas Pequeno', 15, 30.00),
@@ -42,7 +43,11 @@ INSERT INTO habilidades (id_classe, nome_habilidade, descricao, chance_sucesso, 
 INSERT INTO habilidades (id_classe, nome_habilidade, descricao, chance_sucesso, efeito_sucesso_str, efeito_falha_str, efetivo_txt, falha_txt) VALUES
 (5, 'Dado Viciado', '68% de chance de ganhar +40% de chance crítica apenas nesta rodada. Falha: perde 10% do PA apenas nesta rodada.', 68, '+40%CRISEU', '-10%PASSEU', 'A sorte sorri! +40% crítico nesta rodada!','Fumble! Os dados saem ruins, -10% PA apenas nesta rodada.'),
 (5, 'Trapaceiro', '70% de chance de roubar 8% do HP do oponente (dano + cura instantânea) — efeito instantâneo. Falha: perde 10% do HP.', 70, '+8%HPSSEU;-8%HPSADV', '-10%HPSSEU', 'Trapaça perfeita! Rato drena 8% do HP do alvo e ganha a mesma quantidade.','Pego no ato! O rato é punido e perde 10% de HP.'),
+<<<<<<< Updated upstream
 (5, 'All-in', '50% de chance de multiplicar o PA por 3 (PA x3) apenas nesta rodada. Falha: sofre -40% PD e perde 10% do HP.', 50, '+300%PASSEU', '-40%PDSSEU;-10%HPSSEU', 'Tudo ou nada! PA é multiplicado por 4 apenas nesta rodada!','Aposta perdida! -40% PD e -10% HP.');
+=======
+(5, 'All-in', '50% de chance de multiplicar o PA por 3 (PA x3) apenas nesta rodada. Falha: sofre -40% PD e perde 10% do HP.', 50, '+300%PASSEU', '-40%PDSSEU;-10%HPSSEU', 'Tudo ou nada! PA é multiplicado por 3 apenas nesta rodada!','Aposta perdida! -40% PD e -10% HP.');
+>>>>>>> Stashed changes
 
 -- Habilidades Classe 6: Rato de Biblioteca
 INSERT INTO habilidades (id_classe, nome_habilidade, descricao, chance_sucesso, efeito_sucesso_str, efeito_falha_str, efetivo_txt, falha_txt) VALUES
