f4dea101deb7328a9e55baef0a309e0f31a34aca
joaopug <charmandercharmandesco@gmail.com>
2025-11-10 17:55:11 -0300

    Dando continuidade para a tela de histórico

--- FrontEnd/rinhaRatos/src/pages/perfil/TelaHistorico.jsx
+++ FrontEnd/rinhaRatos/src/pages/perfil/TelaHistorico.jsx
@@ -1,12 +1,21 @@
+import { useState } from "react"
+import Vitoria from "../../assets/icones/IconeVitoria.png"
+import Derrota from "../../assets/icones/IconeDerrota.png"
 import "../../pages/perfil/TelaHistorico.css";
 
-export default function TelaHistorico({onClose}) {
+export default function TelaHistorico({ onClose }) {
+
+  const [vencedor, setVencedor] = useState(false)
+
   return (
     <>
       <div className="conteinerHistorico">
         <div className="conteudoHistorico">
-          <h1>Histórico</h1>
-          <button onClick={onClose}>Fechar</button>
+          <h1 className="tituloHistorico">Resultado da Batalha:</h1>
+          <div className={vencedor === true ? "Vitoria" : "Derrota"}>
+            <h2>Você perdeu...</h2>
+          </div>
+          <button className="fecharHistorico" onClick={onClose}>Fechar</button>
         </div>
       </div>
     </>
