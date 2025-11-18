8707eb8fb1d65f0eaa1c7aa2087eca3ffdc1c92c
joaopug <41847866+joaopug@users.noreply.github.com>
2025-11-17 21:03:36 -0300

    Tentando acertar o tamanho das fotos no modal

--- FrontEnd/rinhaRatos/src/pages/perfil/ModalOpcFotosPerfil.jsx
+++ FrontEnd/rinhaRatos/src/pages/perfil/ModalOpcFotosPerfil.jsx
@@ -1,7 +1,31 @@
+import FtPerfilPadrao from "../../assets/perfil/perfil-default.png";
+import FtPerfil1 from "../../assets/perfil/perfil-homem-1.png";
+import FtPerfil2 from "../../assets/perfil/perfil-homem-2.png";
+import FtPerfil3 from "../../assets/perfil/perfil-homem-3.png";
+import FtPerfil4 from "../../assets/perfil/perfil-homem-4.png";
+import FtPerfil5 from "../../assets/perfil/perfil-homem-5.png";
+import FtPerfil6 from "../../assets/perfil/perfil-mulher-1.png";
+import FtPerfil7 from "../../assets/perfil/perfil-mulher-2.png";
+import FtPerfil8 from "../../assets/perfil/perfil-mulher-3.png";
+import FtPerfil9 from "../../assets/perfil/perfil-mulher-4.png";
+import FtPerfil10 from "../../assets/perfil/perfil-mulher-5.png";
 import "../home/jogador/HomeJogador.css";
+import "./ModalOpcFotosPerfil.css";
 
 export default function ModalOpcFoto({ modalAtivado, onClose }) {
-  const listafotosPerfil = []
+  const listafotosPerfil = {
+    1: FtPerfil1,
+    2: FtPerfil2,
+    3: FtPerfil3,
+    4: FtPerfil4,
+    5: FtPerfil5,
+    6: FtPerfil6,
+    7: FtPerfil7,
+    8: FtPerfil8,
+    9: FtPerfil9,
+    10: FtPerfil10,
+    0: FtPerfilPadrao,
+  };
 
   return (
     <>
@@ -10,12 +34,14 @@ export default function ModalOpcFoto({ modalAtivado, onClose }) {
           <button className="sair" onClick={onClose}>
             ✖
           </button>
-        </div>
-        {listafotosPerfil.map((foto) => (
-          <div className="fotoPerfil" key={foto.id}>
-            <img src={foto.url} />
+          <div className="listaFotosPerfil">
+            {Object.values(listafotosPerfil).map((foto) => (
+              <div className="fotoPerfil" key={foto}>
+                <img src={foto} />
+              </div>
+            ))}
           </div>
-        ))  }
+        </div>
       </div>
     </>
   );
