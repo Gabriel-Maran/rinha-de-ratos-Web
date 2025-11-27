b7b23936f26ba0d051a95067d30a71b04aa55b92
Grosbelli12 <gustavogrosbelli11@gmail.com>
2025-11-26 22:53:54 -0300

    API
    chamei a api no home convidado, mas tera que ser trocada

--- FrontEnd/rinhaRatos/src/Api/Api.js
+++ FrontEnd/rinhaRatos/src/Api/Api.js
@@ -162,5 +162,13 @@ export const pegarBatalhasCriadas = (idADM) => {
 };
 
 export const pegarBatalhaPorId = (idBatalha) => {
-    return apiClient.get(`/batalha/${idBatalha}`); 
-}
+  return apiClient.get(`/batalha/${idBatalha}`);
+};
+
+// ---------------------------------------------------------
+// APIS RELACIONADAS AO CONVIDADO
+// ---------------------------------------------------------
+
+export const pegarTodasBatalhasConcluidas = () => {
+  return apiClient.get("batalha/concluidas");
+};
